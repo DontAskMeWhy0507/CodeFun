45816191f48694c9aa9893e6c75503d67be80c92
tuantrunglaiyen <tuantrung0705@gmail.com>
2023-10-09 14:51:02 +0700

    update1nhap

---
+++ draft/test2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int solve(int n, int m)
+{
+    if(n < 0) return 0;
+    if(n == 0) return 1;
+
+    int ans = 0;
+    for (int i = m+1; i <= n; i++) ans += solve(n-i, i);
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << solve(n, 0);
+}
