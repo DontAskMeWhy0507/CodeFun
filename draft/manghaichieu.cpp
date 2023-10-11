ea1f5b8d4cb872fd0d386b89c1e90ee1ea31aae2
tuantrunglaiyen <tuantrung0705@gmail.com>
2023-10-11 17:36:27 +0700

    nhap

--- draft/manghaichieu.cpp
+++ draft/manghaichieu.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
 using namespace std;
-int main(){
-    int i,j;
-    int ncot,mhang,qcauhoi;
-    cin>>mhang>>ncot>>qcauhoi;
-    vector <vector <int> > a(mhang,vector<int> (ncot));
-    for ( i=0;i<mhang;i++){
-        for ( j=0;j<ncot;j++){
-            cin>>a[i][j];
+
+int main() {
+    long long i, j;
+    long long ncot, mhang, qcauhoi;
+    cin >>mhang >> ncot >> qcauhoi;
+    int b[mhang][ncot];
+    for (i = 0; i < mhang; i++) {
+        for (j = 0; j < ncot; j++) {
+            cin >> b[i][j];
         }
     }
-int b[qcauhoi][2];
-int h,k;
-for( h=0;h<qcauhoi;h++){
-    for ( k=0;k<2;k++){
-        cin>>b[h][k];
-    }
-}
-int hang=-1,cot;
-for ( h=0;h<qcauhoi;h++){
-    for(k=0;k<2;k++){
-        cot= b[h][k];
-        if(hang==-1){
-            hang=cot;
+    long long h,k;
+    int a[qcauhoi][2];
+    for (h = 0; h < qcauhoi; h++) {
+        for (k = 0; k < 2; k++) {
+            cin >> a[h][k];
         }
     }
-    cout<<a[hang][cot]<<endl;
-    hang=-1;
-}
-return 0;
+    long long hang=-1, cot;
+    for (h = 0; h < qcauhoi; h++) {
+        for (k = 0; k < 2; k++) {
+            cot = a[h][k]-1;
+            if (hang==-1)
+            {
+                hang = cot;
+            }
+        }
+        cout << b[hang][cot] << endl;
+       
+
+        hang =-1;
+    }
+    return 0;
 }
