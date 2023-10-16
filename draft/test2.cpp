c5ef268f79342cbc99ae02bd0e4e36dba5f54ec5
tuantrunglaiyen <tuantrung0705@gmail.com>
2023-10-16 14:42:30 +0700

    so doi xung

--- draft/test2.cpp
+++ draft/test2.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
-
 using namespace std;
 
-bool isGreaterThanOne(int n) {
-  if(n<10)return false;
-  return n > 1;
+bool isPalindrome(int num) {
+    int reversedNum = 0, originalNum = num;
+    while (num > 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
+    return (originalNum == reversedNum);
 }
 
 int main() {
-  int n ;
-cin>>n;
-  bool isGreater = isGreaterThanOne(n);
+    int n;
+   cout << "Enter the size of the array: ";
+cin >> n;
 
-  cout << "isGreater: " << isGreater << endl;
+int arr[n];
+cout << "Enter the elements of the array: ";
+for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+}
 
-  return 0;
+int countPalindrome = 0;
+for (int i = 0; i < n; i++) {
+    if (isPalindrome(arr[i])) {
+        countPalindrome++;
+    }
+}
+
+if (countPalindrome > 0) {
+    cout << "yes" << endl;
+} else {
+    cout << "no" << endl;
+}
 }
