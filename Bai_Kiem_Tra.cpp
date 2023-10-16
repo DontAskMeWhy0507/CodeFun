cffa1dce3ef04e6ecefd6ece453031acd67067bd
Tuan Trung <62925438+VuTienTuanTrung@users.noreply.github.com>
2023-10-16 17:04:16 +0700

    Update Bai_Kiem_Tra.cpp

--- Bai_Kiem_Tra.cpp
+++ Bai_Kiem_Tra.cpp
@@ -1,1 +1,13 @@
+while (true) //while(0) la gia tri sai, con lai deu la dung.
+{ tong +=n;
+n++;
+if(n==0) break;   // ngat
+//in ra cac so la boi cua 7 <=n
+  int n;
+cin>>n;
+for(int i=7;i<=n; i+=7){
+cout<<i<<" ";}
+// ham bool
+bool flag (false);
+//
 
