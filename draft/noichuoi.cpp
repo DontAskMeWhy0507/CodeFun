debefe4181bbc19369aefe58ad6436f2f739e768
tuantrunglaiyen <tuantrung0705@gmail.com>
2023-10-30 15:14:16 +0700

    dd

--- draft/noichuoi.cpp
+++ draft/noichuoi.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
 string s;
 getline(cin,s);
-int cnt;
+int cnt=0;
 s=" "+s;
 int n=s.length();
 for(int i=0;i<n;i++){
-    if(s[i]==' '&&S[i+1] !=' ') cnt++;
+    if(s[i]==' '&& s[i+1] !=' ') cnt++;
 }
 cout<<cnt;
 }
