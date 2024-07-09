ffd5e7b5400d641fbd685a40f6409ad378bab8a0
Tuan Trung <tuantrung0705@gmail.com>
2024-07-09 21:29:08 +0700

    1234

--- Nhap/1.cpp
+++ Nhap/1.cpp
@@ -1,15 +1,33 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    string s;
-    getline(cin, s);
-    stringstream ss (s);
-    string token;
-    stack<string> st;
-    while(ss >> token)
-    {
-        st.push(token);
-        
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ans = new ListNode(0);
+        ListNode* current = ans;
+        while( list1 && list2)
+        {
+            if(list1->val > list2->val)
+            {
+                current->next = list2;
+                list2 = list2 -> next;
+            }
+            else
+            {
+                current->next = list1;
+                list1 = list1 -> next;
+            }
+            current = current -> next;
+        }
+        current-> next = list1 ? list1 : list2;
+        return ans -> next;
     }
-}
+};
