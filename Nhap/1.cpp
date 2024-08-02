8a971cd7cba9d69835bd4e3977054d2716247e9b
Tuan Trung <tuantrung0705@gmail.com>
2024-08-02 20:06:17 +0700

    1234

--- Nhap/1.cpp
+++ Nhap/1.cpp
@@ -1,58 +1,16 @@
-<<<<<<< HEAD
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ans = new ListNode(0);
-        ListNode* current = ans;
-        while( list1 && list2)
-        {
-            if(list1->val > list2->val)
-            {
-                current->next = list2;
-                list2 = list2 -> next;
-            }
-            else
-            {
-                current->next = list1;
-                list1 = list1 -> next;
-            }
-            current = current -> next;
-        }
-        current-> next = list1 ? list1 : list2;
-        return ans -> next;
-=======
-class Solution {
-public:
-    bool isValid(string s) {
-        stack <char> st;
-        for(char c: s)
-        {
-            if(c=='[' || c == '{' || c == '(')
-            {
-                st.push(c);
-            }
-            else
-            {
-                if  (st.empty() ||
-                    (c == ')' && st.top() != '(') ||
-                    (c == ']' && st.top() != ']') ||
-                    (c == '}' && st.top() != '}')
-                ) return false;
-                st.pop();
-            }
-        }
-                    return st.empty();
+    int myAtoi(string s) {
+        int i = 0, sign = 1;
+        long asn = 0;
+        while(s[i] == ' ') i++;
+        if(s[i] == '+' || s[i] == '-') sign = 1 - 2 * (s[i++] == '-');
 
->>>>>>> 53fd8e330602366f9fb3856809c9d1c362419e1c
+        while(s[i] >= '0' && s[i] <= '9') {
+            asn = asn * 10 + (s[i++] - '0');
+
+        }
+        asn *= sign;
+        return asn;
     }
 };
