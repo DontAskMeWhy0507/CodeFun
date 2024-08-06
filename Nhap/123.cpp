3ce33d9fe62c0101a6d2033b240fee7af5f96392
Tuan Trung <tuantrung0705@gmail.com>
2024-08-06 18:14:26 +0700

    234523

--- Nhap/123.cpp
+++ Nhap/123.cpp
@@ -1,68 +1,24 @@
-#include <iostream>
-using namespace std;
-
-#define N 10
-#define M 10
-
-class CaroGame {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    int board[N][M];
-    int n;
-    int m;
-    int player;
-    int opponent;
-
-    CaroGame(int n, int m) {
-        this->n = n;
-        this->m = m;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = 0;
-            }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* reverse = new ListNode();
+        ListNode* current = head;
+        ListNode* tmp = new ListNode();
+        while(current != nullptr)
+        {
+            tmp 
         }
-        player = 1;
-        opponent = 2;
-    }
-
-    void printBoard() {
-        //implement this
-    }
-
-    bool isMoveValid(int x, int y) {
-       //implement this
-    }
-
-    bool isWin(int x, int y) {
-       //implement this
-    }
-
-    bool tick(int x, int y) {
-       //implement this
-    }
-
-    void run() {
-        while (true) {
-            printBoard();
-            cout << "Player " << player << " input move:" << endl;
-            int x, y;
-            cin >> x >> y;
-
-            if (isMoveValid(x, y)) {
-                bool isEndGame = tick(x, y);
-                if (isEndGame) {
-                    printBoard();
-                    break;
-                }
-            } else {
-                cout << "Invalid move" << endl;
-            }
-        }
+        reverse -> next = nullptr;
+        return reverse;
     }
 };
-
-int main() {
-    CaroGame caroGame(10, 10);
-    caroGame.run();
-    return 0;
-}
-
--- Nhap/2.cpp
+++ Nhap/2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void print(a[], n)
-{
-    for (int i)
-}
+ int main()
+ {
+    
+ }
