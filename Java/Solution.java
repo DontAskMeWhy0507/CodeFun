8ddfe651a1b23b59b3dfa5e03cdb877044ee17db
Tuan Trung <62925438+VuTienTuanTrung@users.noreply.github.com>
2024-10-09 21:46:34 +0700

    43245

--- Java/Solution.java
+++ Java/Solution.java
@@ -16,11 +16,14 @@ public class Solution {
         this.numerator = numerator;
     }
 
-    public int getDemoniator() {
+    public int getDenominator() {
         return denominator;
     }
 
-    public void setDemoniator(int denominator) {
+    /**
+     * Set the denominator.
+     */
+    public void setDenominator(int denominator) {
         if (denominator != 0) {
             this.denominator = denominator;
         }
@@ -29,39 +32,116 @@ public class Solution {
     /**
      * Constructor.
      */
+    public Solution(int numerator, int denominator) {
+        if (denominator != 0) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        } else {
+            this.numerator = numerator;
+            this.denominator = 1;
+        }
+    }
 
-     public Solution(int numerator, int denominator) {
-         this.numerator = numerator;
-         this.denominator = denominator;
-     }
+    int ucln(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            return Math.abs(a + b);
+        }
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+        return Math.abs(a);
+    }
 
-     int ucln(int a, int b) {
-         a = Math.abs(a);
-         b = Math.abs(b);
-         if (a == 0 || b == 0) {
-             return Math.abs(a + b);
-         }
-         while (a != b) {
-             if (a > b) {
-                 a -= b;
-             } else {
-                 b -= a;
-             }
-         }
-         return Math.abs(a);
-     }
+    /**
+     * Trung dep trai.  
+     */
+    public Solution reduce() {
+        int ucln = ucln(numerator, denominator);
+        if (ucln == 0) {
+            return this;
+        }
+        this.numerator /= ucln;
+        this.denominator /= ucln;
+        return this;
+    }
 
     /**
      * Trung dep trai.
+     * @return deptrai
      */
-     public void reduce() {
-        int ucln = ucln(numerator, denominator);
-        numerator /= ucln;
-        denominator /= ucln;
-     }
 
-     /**
-      * Trung dep trai.
-      */
-      
+    public Solution multiply(Solution other) {
+        if (other.denominator == 0) {
+            return this;
+        }
+        int num = numerator * other.numerator;
+        int den = denominator * other.denominator;
+        this.numerator = num;
+        this.denominator = den;
+        return this.reduce();
+    }
+
+    /**
+     * Trung dep trai.
+     * @return deptrai
+     */
+    public Solution add(Solution other) {
+        if (other.denominator == 0) {
+            return this;
+        }
+        int num = numerator * other.denominator + other.numerator * denominator;
+        int den = denominator * other.denominator;
+        this.numerator = num;
+        this.denominator = den;
+        return this.reduce();
+        
+    }
+
+    /**
+     * Trung dep trai.
+     * @return deptrai
+     */
+    public Solution subtract(Solution other) {
+        if (other.denominator == 0) {
+            return this;
+        }
+        int num = numerator * other.denominator - other.numerator * denominator;
+        int den = denominator * other.denominator;
+        this.numerator = num;
+        this.denominator = den;
+        return this.reduce();
+    }
+
+    /**
+     * Trung dep trai.
+     * @return deptrai
+     */
+    public Solution divide(Solution other) {
+        if (other.denominator == 0) {
+            return this;
+        }
+        int num = numerator * other.denominator;
+        int den = denominator * other.numerator;
+        this.numerator = num;
+        this.denominator = den;
+        return this.reduce();
+    }
+
+    /**
+     * Trung dep trai.
+     * @return deptrai
+     */
+    public boolean equals(Object obj) {
+        if (obj instanceof Solution) {
+            Solution other = (Solution) obj;
+            return (numerator * other.denominator) == (other.numerator * denominator);
+        }
+        return false;
+    }
 }
